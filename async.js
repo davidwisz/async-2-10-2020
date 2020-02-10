15de3cd7c7cc106920b35576583287bbb71176be
DAVID WISZ <david@wisz.com>
2020-02-10 15:43:49 -0800

    added Gimme Card button and functionality

--- async.js
+++ async.js
@@ -49,8 +49,54 @@ function getNumberCallback(num){
     // return output
    
 }
+let counter = 1;
+let deckId = 0;
+function getNewDeck(){
+    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(response => response.data)
+    .then(data => deckId = data.deck_id)
+    .then(function(){
+        let button = document.createElement('button');
+        button.innerHTML = 'GIMME A CARD!';
+        button.id = 'card-getter';
+        document.getElementById('content').appendChild(button);
+    });
+}
+function getCard(){
+    axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then(resp => resp.data)
+    .then(cardData => cardData.cards[0])
+    .then(function(theCard) { 
+        counter++; 
+        let imageEl = document.createElement('img');
+        imageEl.src = theCard.image;
+        randomDegrees = Math.floor(Math.random() * Math.floor(45));
+        randomDirection = Math.random() < .5 ? '-' : '';
+        imageEl.style.transform = `rotate(${randomDirection}${randomDegrees}deg)`;
+        document.getElementById('content').appendChild(imageEl)});
+        console.log(counter);
+        if (counter === 52) {
+            document.getElementById('card-getter').style.display = 'none';
+        }
+    //console.log(deckid)
+}
+function getTwoCards(){
+    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(response => response.data)
+    .then(data => data.deck_id)
+    .then(deckid => axios.get(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=2`))
+    .then(resp => resp.data)
+    .then(cardData => cardData.cards)
+    .then(function(theCards) { for (card in theCards) { console.log(`${theCards[card].value} of ${theCards[card].suit}`)}});
+    //console.log(deckid)
+}
 
 window.onload = function(){
-
-    getNumberCallback([1,4,7]);
+    getNewDeck();
+    document.getElementById('body').addEventListener('click', function(e){
+        let target = e.target;
+        if (e.target.id == 'card-getter') {
+            getCard();
+        }
+    })
 };
