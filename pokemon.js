382aa6982fa5ac1e6cbfae9019742f05f475714a
DAVID WISZ <david@wisz.com>
2020-02-10 16:30:55 -0800

    starting pokemon exercises

---
+++ pokemon.js
@@ -0,0 +1,33 @@
+async function initiatePokemon() {
+    let resp = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=964');
+    let pokemons = resp.data.results;
+    
+    r1 = Math.floor(Math.random() * Math.floor(964));
+    r2 = r1 < 3 ? r1 + 3 : r1 - 3;
+    r3 = r1 < 3 ? r2 + 3 : r2 - 3;
+    let resp1 = axios.get(pokemons[r1].url);
+    let resp2 = axios.get(pokemons[r2].url);
+    let resp3 = axios.get(pokemons[r3].url);
+    
+    let pokemonPromise = await Promise.all(
+        [resp1, resp2, resp3]
+    );
+    let pokes = [];
+    for (p in pokemonPromise) {
+        pokes.push(pokemonPromise[p].data);
+    }
+    let spec1 = axios.get(pokes[0].species.url);
+    let spec2 = axios.get(pokes[1].species.url);
+    let spec3 = axios.get(pokes[2].species.url);
+    
+    let speciesPromise = await Promise.all(
+        [spec1, spec2, spec3]
+    );
+
+    //console.log(pokemonPromise)
+    
+}
+
+window.onload = async function(){
+    await initiatePokemon()
+}
